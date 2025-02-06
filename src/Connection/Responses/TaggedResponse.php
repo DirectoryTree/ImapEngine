d1aaa83b7c224a77ad8a8f87294bc5e3ce853acc
Steve Bauman <steven_bauman@outlook.com>
2025-02-06 00:11:33 -0500

    Update TaggedResponse.php

--- src/Connection/Responses/TaggedResponse.php
+++ src/Connection/Responses/TaggedResponse.php
@@ -2,13 +2,47 @@
 
 namespace DirectoryTree\ImapEngine\Connection\Responses;
 
+use DirectoryTree\ImapEngine\Connection\Tokens\Atom;
+
 class TaggedResponse extends Response
 {
     /**
-     * Get the response tag.
+     * Get the response token.
      */
-    public function tag(): ?string
+    public function tag(): ?Atom
     {
         return $this->tokens[0] ?? null;
+    }
+
+    /**
+     * Get the status token.
+     */
+    public function status(): ?Atom
+    {
+        return $this->tokens[1] ?? null;
+    }
+
+    /**
+     * Get the response messages.
+     */
+    public function messages(): array
+    {
+        return array_slice($this->tokens, 2);
+    }
+
+    /**
+     * Determine if the response was successful.
+     */
+    public function successful(): bool
+    {
+        return $this->status()?->value === 'OK';
+    }
+
+    /**
+     * Determine if the response failed.
+     */
+    public function failed(): bool
+    {
+        return in_array($this->status()?->value, ['NO', 'BAD']);
     }
 }
