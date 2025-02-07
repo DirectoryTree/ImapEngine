cdead2f8dce335774d89a4ed934c6631f3ab3927
Steve Bauman <steven_bauman@outlook.com>
2025-02-07 02:09:33 -0500

    Add ability to determine atom values

--- src/Connection/Tokens/Atom.php
+++ src/Connection/Tokens/Atom.php
@@ -2,4 +2,21 @@
 
 namespace DirectoryTree\ImapEngine\Connection\Tokens;
 
-class Atom extends Token {}
+class Atom extends Token
+{
+    /**
+     * Determine if the token is the given value.
+     */
+    public function is(string $value): bool
+    {
+        return $this->value === $value;
+    }
+
+    /**
+     * Determine if the token is not the given value.
+     */
+    public function isNot(string $value): bool
+    {
+        return ! $this->is($value);
+    }
+}
