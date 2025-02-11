8c28c55267decbcf7835d7a3f6f633d1db6d9a9d
Steve Bauman <steven_bauman@outlook.com>
2025-02-10 23:13:47 -0500

    Add find and findOrFail methods on message collection

--- src/Collections/MessageCollection.php
+++ src/Collections/MessageCollection.php
@@ -2,4 +2,27 @@
 
 namespace DirectoryTree\ImapEngine\Collections;
 
-class MessageCollection extends PaginatedCollection {}
+use DirectoryTree\ImapEngine\Message;
+
+class MessageCollection extends PaginatedCollection
+{
+    /**
+     * Find a message by its UID.
+     */
+    public function find(int $uid): ?Message
+    {
+        return $this->first(
+            fn (Message $message) => $message->uid() === $uid
+        );
+    }
+
+    /**
+     * Find a message by its UID or throw an exception.
+     */
+    public function findOrFail(int $uid): Message
+    {
+        return $this->firstOrFail(
+            fn (Message $message) => $message->uid() === $uid
+        );
+    }
+}
