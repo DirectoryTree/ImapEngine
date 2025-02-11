fe06d904fbdb2085d0867d03ab052589da64c5a5
Steve Bauman <steven_bauman@outlook.com>
2025-02-10 19:50:04 -0500

    Add mailbox integration test

---
+++ tests/Integration/MailboxTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use DirectoryTree\ImapEngine\Folder;
+
+test('connect', function () {
+    $mailbox = mailbox();
+
+    $mailbox->connect();
+})->throwsNoExceptions();
+
+test('inbox', function () {
+    $mailbox = mailbox();
+
+    expect($mailbox->inbox())->toBeInstanceOf(Folder::class);
+});
+
+test('capabilities', function () {
+    $mailbox = mailbox();
+
+    expect($mailbox->capabilities())->toBe(['CAPABILITY', 'IMAP4rev1', 'LITERAL+', 'UIDPLUS', 'SORT', 'IDLE', 'MOVE', 'QUOTA']);
+});
