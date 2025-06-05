45b48904894103809a2ab83b58cea10c6bcb56a8
Steve Bauman <steven_bauman@outlook.com>
2025-06-05 18:04:09 -0400

    Fix test

--- tests/Unit/Connection/ImapQueryBuilderTest.php
+++ tests/Unit/Connection/ImapQueryBuilderTest.php
@@ -242,5 +242,5 @@ test('compiles multiple UID values without quotes', function () {
 
     $builder->uid([2, 3, 5]);
 
-    expect($builder->toImap())->toBe('UID 2,3');
+    expect($builder->toImap())->toBe('UID 2,3,5');
 });
