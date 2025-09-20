ee6663607ac962c8319eba4827e9f898d3a5d241
Steve Bauman <steven_bauman@outlook.com>
2025-09-20 16:51:02 -0400

    Reduce width

--- src/Folder.php
+++ src/Folder.php
@@ -161,7 +161,9 @@ class Folder implements Arrayable, FolderInterface, JsonSerializable
     public function quota(): array
     {
         if (! in_array('QUOTA', $this->mailbox->capabilities())) {
-            throw new ImapCapabilityException('Unable to fetch mailbox quotas. IMAP server does not support QUOTA capability.');
+            throw new ImapCapabilityException(
+                'Unable to fetch mailbox quotas. IMAP server does not support QUOTA capability.'
+            );
         }
 
         $responses = $this->mailbox->connection()->quotaRoot($this->path);
