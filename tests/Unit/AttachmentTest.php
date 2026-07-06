dfeaf94f8223bf5ec3a61e41ec6bdbea32e80bc7
Steve Bauman <steven_bauman@outlook.com>
2026-07-06 12:07:15 -0400

    Fix repeated attachment content reads

--- tests/Unit/AttachmentTest.php
+++ tests/Unit/AttachmentTest.php
@@ -1,10 +1,10 @@
 <?php
 
 use DirectoryTree\ImapEngine\Attachment;
-use GuzzleHttp\Psr7\LazyOpenStream;
+use GuzzleHttp\Psr7\Utils;
 
 test('extension', function () {
-    $stream = new LazyOpenStream('test.jpg', 'r');
+    $stream = Utils::streamFor('');
 
     $ext = (new Attachment('test.jpg', null, 'image/jpeg', 'attachment', $stream))->extension();
 
@@ -12,9 +12,33 @@ test('extension', function () {
 });
 
 test('extension with content type', function () {
-    $stream = new LazyOpenStream('test', 'r');
+    $stream = Utils::streamFor('');
 
     $ext = (new Attachment('test', null, 'image/jpeg', 'attachment', $stream))->extension();
 
     expect($ext)->toBe('jpg');
+});
+
+test('contents can be read multiple times', function () {
+    $stream = Utils::streamFor('Hello World!');
+
+    $attachment = new Attachment('test.txt', null, 'text/plain', 'attachment', $stream);
+
+    expect($attachment->contents())->toBe('Hello World!');
+    expect($attachment->contents())->toBe('Hello World!');
+});
+
+test('save writes contents after reading contents', function () {
+    $stream = Utils::streamFor('Hello World!');
+
+    $attachment = new Attachment('test.txt', null, 'text/plain', 'attachment', $stream);
+
+    $path = tempnam(sys_get_temp_dir(), 'imap-engine-attachment-');
+
+    $attachment->contents();
+    $attachment->save($path);
+
+    expect(file_get_contents($path))->toBe('Hello World!');
+
+    unlink($path);
 });
