588955494a491368ec01ceeffa39637b5b940a55
Steve Bauman <steven_bauman@outlook.com>
2026-09-20 11:19:46 -0400

    Sort cases

--- src/MessageData/Attribute.php
+++ src/MessageData/Attribute.php
@@ -6,8 +6,8 @@ enum Attribute: string implements FetchItemInterface
 {
     case Flags = 'FLAGS';
     case Size = 'RFC822.SIZE';
-    case BodyStructure = 'BODYSTRUCTURE';
     case ModSequence = 'MODSEQ';
+    case BodyStructure = 'BODYSTRUCTURE';
 
     /**
      * {@inheritDoc}
